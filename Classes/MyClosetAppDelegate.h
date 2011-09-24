228663fa566402b44874f17a4be9a70e79360dda
leon cheng <leoncheng0528@hotmail.com>
2011-09-24 20:01:08 +0800

    initial setup

---
+++ Classes/MyClosetAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  MyClosetAppDelegate.h
+//  MyCloset
+//
+//  Created by mmcl on 11-9-24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyClosetAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
